03a773c8a00a021c59b4501d1df2cddc6591cebb
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-18 18:14:16 +0200

    feat: Add album creation from edit artist admin page

--- artists/admin.py
+++ artists/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Artist
+from albums.models import Album
+
+
+class AlbumInline(admin.TabularInline):
+    model = Album
+    extra = 1
 
 
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ('stage_name', 'social_link')
+    inlines = [AlbumInline]
 
 
 # Register your models here.
