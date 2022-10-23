f1fa40213ea4db44909205ff67afd63c894f4bf7
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-23 15:45:21 +0200

    feat: Change album form to use user friendly date input widget

--- albums/forms.py
+++ albums/forms.py
@@ -1,11 +1,18 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Album
 
 
-class AlbumForm(ModelForm):
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
+class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
         fields = ('name', 'artist', 'release_date', 'cost', 'is_approved')
         help_texts = {
             'is_approved': 'Approve the album if its name is not explicit'
+        }
+        widgets = {
+            'release_date': DateInput
         }
