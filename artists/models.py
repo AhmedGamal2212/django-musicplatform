abe1f783167bee5f70a941375f8d2023f0889a1b
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-23 12:26:28 +0200

    Add TODO to artists/models

--- artists/models.py
+++ artists/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-# Create your models here.
+# TODO: order by approved albums.
+# class ArtistManager(models.Manager):
+#     def get_query_set(self):
+#         params = self.request.query_params
+#         if params.contains('approved_albums'):
+#             return super().get_queryset().filter(is_approved=True).count()
+
 class Artist(models.Model):
     stage_name = models.CharField(max_length=255, unique=True)
     social_link = models.URLField(max_length=254, null=False, blank=True)
