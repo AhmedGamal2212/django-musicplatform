818ed17b09e26db171ac9ba305d1e207ca400f12
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-28 20:12:33 +0200

    fix: Fix validate_audio function

--- albums/validators/validators.py
+++ albums/validators/validators.py
@@ -3,9 +3,9 @@ from django.core.exceptions import ValidationError
 
 def validate_audio(value):
     value = str(value)
-    ok = True
-    ok &= value.endswith('.wav')
-    ok &= value.endswith('.mp3')
+    ok = False
+    ok |= value.endswith('.wav')
+    ok |= value.endswith('.mp3')
 
     if not ok:
         raise ValidationError('only .wav/.mp3 files allowed')
