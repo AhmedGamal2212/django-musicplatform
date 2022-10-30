a389cf1cd877c418abbc10d1b118517105d17356
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-30 16:55:43 +0200

    feat: Add logout/logoutall functionalities from knox authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from knox import views as knox_views
 
 urlpatterns = [
     path('register/', views.register_api),
     path('login/', views.login_api),
+    path('logout/', knox_views.LogoutView.as_view()),
+    path('logoutall/', knox_views.LogoutAllView.as_view())
 ]
