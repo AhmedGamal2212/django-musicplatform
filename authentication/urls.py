a1fe52c050bd6b94a13be23a5e68fdea62c0b840
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-10-30 17:35:23 +0200

    refactor: Change the authentication apis to class based api views

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,8 +3,8 @@ from . import views
 from knox import views as knox_views
 
 urlpatterns = [
-    path('register/', views.register_api),
-    path('login/', views.login_api),
+    path('register/', views.RegisterAPI.as_view()),
+    path('login/', views.LoginAPI.as_view()),
     path('logout/', knox_views.LogoutView.as_view()),
     path('logoutall/', knox_views.LogoutAllView.as_view())
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,43 +1,45 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.auth import AuthToken
 from .serializers import RegisterSerializer
+from rest_framework import generics
 
 
-# TODO: change this view to class based
-@api_view(['POST'])
-def login_api(request):
-    serializer = AuthTokenSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    user = serializer.validated_data['user']
-    _, token = AuthToken.objects.create(user)
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = AuthTokenSerializer
 
-    return Response({
-        'token': token,
-        'user': {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'bio': user.bio,
-        },
-    })
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        _, token = AuthToken.objects.create(user)
+
+        return Response({
+            'token': token,
+            'user': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+                'bio': user.bio,
+            },
+        })
 
 
-# TODO: change this view to class based
-@api_view(['POST'])
-def register_api(request):
-    serializer = RegisterSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
 
-    user = serializer.save()
-    _, token = AuthToken.objects.create(user)
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-    return Response({
-        'token': token,
-        'user': {
-            'username': user.username,
-            'email': user.email,
-            'bio': user.bio
-        }
-    })
+        user = serializer.save()
+        _, token = AuthToken.objects.create(user)
+
+        return Response({
+            'token': token,
+            'user': {
+                'username': user.username,
+                'email': user.email,
+                'bio': user.bio
+            }
+        })
