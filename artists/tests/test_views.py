bbc9ceb33134845bad5aa3248563deb146faeb02
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-01 23:54:25 +0200

    feat: Add test for creating an artist using unauthorized user

--- artists/tests/test_views.py
+++ artists/tests/test_views.py
@@ -9,19 +9,29 @@ def perform_get(client):
         social_link='https://google.com'
     )
     response = client.get('/artists/')
-
     assert response.status_code == 200
     assert len(response.data) == Artist.objects.all().count()
     assert response.data[0]['id'] == artist.id
 
 
 @pytest.mark.django_db
-def test_get_users_unauthorized():
+def test_get_artists_unauthorized():
     client = APIClient()
     perform_get(client)
 
 
 @pytest.mark.django_db
-def test_get_users_authorized(auth_client):
+def test_get_artists_authorized(auth_client):
     client = auth_client()
     perform_get(client)
+
+
+@pytest.mark.django_db
+def test_create_artist_unauthorized():
+    client = APIClient()
+    response = client.post('/artists/', {
+        'stage_name': 'ahmed gamal',
+        'social_link': 'https://google.com'
+    })
+    assert response.status_code == 401
+
