012a115f79c6ef841cfa6471a22183ceca09b764
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-02 00:26:24 +0200

    feat: Add a test for register without an optional field

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -27,3 +27,15 @@ def test_register_without_a_required_field():
     })
     assert response.status_code == 400
 
+
+@pytest.mark.django_db
+def test_register_without_optional_field():
+    client = APIClient()
+    response = client.post('/authentication/register/', {
+        'username': 'gemmy in testing',
+        'email': 'gemmytesting@testing.com',
+        'password1': 'Ahmed2212@',
+        'password2': 'Ahmed2212@',
+        'bio': ''
+    })
+    assert response.status_code == 400
