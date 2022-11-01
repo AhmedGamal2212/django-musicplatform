cb89b5acc855fa5972e5c041f4b95cd60d09e71e
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-01 21:24:25 +0200

    feat: Add pytest, pytest-django configs, Create global auth_client fixture

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from rest_framework.test import APIClient
+from users.models import CustomUser
+from knox.auth import AuthToken
+
+testing_user = {
+    'username': 'testing user',
+    'email': 'testinguser@testing.com',
+    'password': 'testing123'
+}
+
+
+@pytest.fixture
+def auth_client(user=None):
+    client = APIClient()
+    if user is None:
+        user = CustomUser.objects.create_user(
+            username=testing_user['username'],
+            email=testing_user['email'],
+            password=testing_user['password'],
+        )
+
+    _, token = AuthToken.objects.create(user)
+    token = 'Token ' + token
+    client.credentials(HTTP_AUTHORIZATION=token)
+
+    return client
