6567f7cf5596a63b9cc81a7bd4da6c2467673fb9
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-02 00:27:48 +0200

    fix: Fix a bug in post request in the last test

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -32,10 +32,10 @@ def test_register_without_a_required_field():
 def test_register_without_optional_field():
     client = APIClient()
     response = client.post('/authentication/register/', {
-        'username': 'gemmy in testing',
+        'username': 'gemmyintesting',
         'email': 'gemmytesting@testing.com',
         'password1': 'Ahmed2212@',
         'password2': 'Ahmed2212@',
         'bio': ''
     })
-    assert response.status_code == 400
+    assert response.status_code == 200
