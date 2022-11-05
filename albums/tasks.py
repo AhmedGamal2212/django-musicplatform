a3473df6e91f7a93ed91dffc47419bde073d1c59
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-05 21:02:49 +0200

    feat: Add a task for sending the 30 days inactivity mail

--- albums/tasks.py
+++ albums/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from artists.models import Artist
 from musicplatform import settings
@@ -9,7 +10,7 @@ from musicplatform import settings
 def send_congratulations_mail(self, album_name, artist_name):
     mail_subject = 'Congratulations!'
     artist = Artist.objects.get(stage_name=artist_name)
-    message = f"Hi {artist_name}, Congratulations on the new album!.\nYou have successfully created a new album: {album_name}"
+    message = f'Hi {artist_name}, Congratulations on the new album!.\nYou have successfully created a new album: {album_name}'
     to_email = artist.user.email
     send_mail(
         subject=mail_subject,
@@ -18,4 +19,32 @@ def send_congratulations_mail(self, album_name, artist_name):
         recipient_list=[to_email],
         fail_silently=True,
     )
-    return "Done"
+    return 'Sent'
+
+
+@shared_task(bind=True)
+def check_for_added_albums(self):
+    all_artists = Artist.objects.add()
+    not_active = []
+    for artist in all_artists:
+        if artist.album_set.count():
+            latest_album = artist.album_set.latest('release_date')
+            if timezone.now() - latest_album.release_date >= timezone.timedelta(days=30):
+                not_active.append(artist)
+
+    for artist in not_active:
+        mail_subject = 'Warning!'
+        artist = Artist.objects.get(stage_name=artist.stage_name)
+        message = f'''
+            Hi {artist.stage_name}, We noticed that you have not created any new album in the last 30 days,
+            the continuous inactivity will cause your popularity on the platform to decrease.
+         '''
+        to_email = artist.user.email
+        send_mail(
+            subject=mail_subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[to_email],
+            fail_silently=True,
+        )
+    return 'Sent'
