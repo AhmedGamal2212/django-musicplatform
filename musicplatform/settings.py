a21df84d178950833572a0fd8b382584e07e8d3e
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2022-11-05 22:04:19 +0200

    docs: Add secret key to environment variables

--- musicplatform/settings.py
+++ musicplatform/settings.py
@@ -22,7 +22,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--yo&cup$wh3__f*=(+$rlzr659vz9d_o=vyxacgyq4euu50dp_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
